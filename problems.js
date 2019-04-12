89e2a9bb6cc8c078228555c32652b60328f43d92
Daniel Goldman <dzgoldman@wesleyan.edu>
2019-04-12 08:36:37 -0400

    update problems

--- problems.js
+++ problems.js
@@ -4,18 +4,29 @@
 // You will have to figure out what parameters to include
 // All functions must use recursion
 
-function findMax(){
-    // This function returns the largest number in a given array.
-}
+
 
 function factorial(){
+    
     // This function returns the factorial of a given number.
 }
+
+
+
 
 function fibonacci(){
     // This function returns the Nth number in the fibonacci sequence.
     // https://en.wikipedia.org/wiki/Fibonacci_number
     // For this function, the first two fibonacci numbers are 1 and 1
+}
+
+function findMax(arr){
+    // This function returns the largest number in a given array.
+}
+
+
+function sumArray(arr) {
+
 }
 
 function coinFlips(){
@@ -34,4 +45,6 @@ function letterCombinations(){
     // ["a","b","c","ab","ac","ba","bc","ca","cb","abc","acb","bac","bca","cab","cba"]
 }
 // Test Script below, DO NOT TOUCH 
+
+
 
