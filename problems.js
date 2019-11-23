8918c9759b435bfe66a45c64d79d03bc2da9bec1
dan-goldman <daniel.goldman@generalassemb.ly>
2019-11-23 09:09:27 -0500

    export problems

--- problems.js
+++ problems.js
@@ -50,7 +50,14 @@ function letterCombinations(){
     // For example, letterCombinations(["a","b","c"]) would return the following:
     // ["a","b","c","ab","ac","ba","bc","ca","cb","abc","acb","bac","bca","cab","cba"]
 }
-// Test Script below, DO NOT TOUCH 
 
 
-
+module.exports = {
+    factorial,
+    fibonacci,
+    reverseString,
+    sumArray,
+    findMax,
+    coinFlips,
+    letterCombinations
+}
