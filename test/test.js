1237b253d225d0606ff444e18666ce47a50b5a96
dan-goldman <daniel.goldman@generalassemb.ly>
2019-11-23 08:24:20 -0500

    add tests

--- test/test.js
+++ test/test.js
@@ -1,8 +1,61 @@
 const assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
+const fns = require('../problems.js')
+
+describe('#factorial()', function() {
+    it('handles base case', function() {
+        assert.equal(fns.factorial(1), 1)
     });
-  });
+    it('calculates factorials', function() {
+        assert.equal(fns.factorial(5), 120)
+        assert.equal(fns.factorial(8), 40320)
+
+    });
 });
+
+describe('#fibonacci()', function() {
+    it('handles base cases', function() {
+        assert.equal(fns.fibonacci(1), 1)
+        assert.equal(fns.fibonacci(2), 1)
+
+    });
+    it('calculates fibonacci numbers', function() {
+        assert.equal(fns.fibonacci(5), 5)
+        assert.equal(fns.fibonacci(10), 55)
+        assert.equal(fns.fibonacci(30), 832040)
+    });
+});
+
+describe('#reverseString()', function() {
+    it('handles base cases', function() {
+        assert.equal(fns.reverseString(""),"")
+        assert.equal(fns.reverseString("x"), "x")
+
+    });
+    it('reverses strings', function() {
+        assert.equal(fns.reverseString("abcdefg"), "gfedcba")
+        assert.equal(fns.reverseString("123"), "321")
+    });
+});
+describe('#sumArray()', function() {
+    it('handles base case', function() {
+        assert.equal(fns.sumArray([1]),1)
+    });
+    it('sums array values', function() {
+        assert.equal(fns.sumArray([1,1]),2)
+        assert.equal(fns.sumArray([1,2,3,4,5]),15)
+    });
+});
+
+describe('#findMax()', function() {
+    it('handles base case', function() {
+        assert.equal(fns.findMax([1]),1)
+    });
+    it('finds max value', function() {
+        assert.equal(fns.findMax([1,1]),1)
+        assert.equal(fns.findMax([2,1]),2)
+        assert.equal(fns.findMax([1,2]),2)
+        assert.equal(fns.findMax([1,4,2,5,6,3,12,1]),12)
+
+    });
+});
+
