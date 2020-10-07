80931dc4d0c5bebaed88231613814ab6c19a3b60
James Mernin <jamespmernin@gmail.com>
2020-10-07 11:23:34 -0400

    Finished all algorithms

--- problems.js
+++ problems.js
@@ -1,78 +1,107 @@
 // Copy from myga:
-
 // Write code inside the functions
 // You will have to figure out what parameters to include
 // All functions must use recursion
 
-
-
-function factorial(){
-    
-    // This function returns the factorial of a given number.
+function factorial(x) {
+  // This function returns the factorial of a given number.
+  return x === 0 ? 1 : x * factorial(x - 1);
 }
 
-
-
-
-function fibonacci(){
-    // This function returns the Nth number in the fibonacci sequence.
-    // https://en.wikipedia.org/wiki/Fibonacci_number
-    // For this function, the first two fibonacci numbers are 1 and 1
+function fibonacci(x) {
+  // This function returns the Xth number in the fibonacci sequence.
+  // https://en.wikipedia.org/wiki/Fibonacci_number
+  // For this function, the first two fibonacci numbers are 1 and 1
+  return x < 2 ? x : fibonacci(x - 1) + fibonacci(x - 2);
 }
 
-function fizzBuzz(){
-    // Write a function takes a number n as a paramter and, for each number from 1 to n, prints "fizz" if the number is divisible by 3, "buzz" if the number is divisible by 5, and "fizzBuzz" if the number is divisible by both 3 and 5.
+function fizzBuzz(x) {
+  // Write a function takes a number x as a parameter and, for each number from 1 to x, prints "fizz" if the number is divisible by 3, "buzz" if the number is divisible by 5, and "fizzBuzz" if the number is divisible by both 3 and 5.
+  if (x === 1) {
+    return '1';
+  } else {
+    let fizzer = (x % 15 === 0) ? ', fizzBuzz' :
+      (x % 5 === 0) ? ', buzz' :
+        (x % 3 === 0) ? ', fizz' :
+          `, ${x}`;
+    return fizzBuzz(x - 1) + fizzer;
+  }
 }
 
-function reverseString() {
-	// This function takes a string and reverses it recursively.
+function reverseString(s) {
+  // This function takes a string and reverses it recursively.
+  return s === '' ? s : reverseString(s.slice(1)) + s[0];
 }
 
-function multiplication (){
-    // this function takes in two numbers and returns their product (assume both numbers are non-negative)
+function multiplication(x, y) {
+  // this function takes in two numbers and returns their product (assume both numbers are non-negative)
+  if (x < y) {
+    return multiplication(y, x);
+  } else if (y !== 0) {
+    return (x + multiplication(x, y - 1));
+  } else {
+    return 0;
+  }
 }
 
-
-function sumArray() {
-    // this function returns the sum of all of the numbers is a given array 
+function sumArray(a) {
+  // this function returns the sum of all of the numbers is a given array
+  return a.length === 1 ? a[0] : a[0] + sumArray(a.slice(1));
 }
 
-function findInArray(){
-       // this function takes an array of strings and a target string, and returns a boolean indicating whether the target string is included in the array
+function findInArray(a, s) {
+  // this function takes an array of strings and a target string, and returns a boolean indicating whether the target string is included in the array
+  return a.length === 0 ? false : a[0] === s ? true : findInArray(a.slice(1), s);
 }
 
-function findMax(){
-    // This function returns the largest number in a given array.
+function findMax(a) {
+  // This function returns the largest number in a given array.
+  return a.length === 1 ? a[0] : (a[0] > findMax(a.slice(1)) ? a[0] : findMax(a.slice(1)));
 }
 
-
-
-function coinFlips(){
-    // This function returns an array of all possible outcomes from flipping a coin N times.
-    // Input type: Integer
-    // For example, coinFlips(2) would return the following:
-    // ["HH", "HT", "TH", "TT"]
-    // H stands for Heads and T stands for tails
-    // Represent the two outcomes of each flip as "H" or "T"
+function coinFlips(x) {
+  // This function returns an array of all possible outcomes from flipping a coin x times.
+  // Input type: Integer
+  // For example, coinFlips(2) would return the following:
+  // ["HH", "HT", "TH", "TT"]
+  // H stands for Heads and T stands for tails
+  // Represent the two outcomes of each flip as "H" or "T"
+  return x <= 0 ? [''] : coinFlips(x - 1).flatMap(r => [r + 'H', r + 'T']);
 }
 
-function letterCombinations(){
-    // This function returns an array of all combinations of the given letters
-    // Input type: Array of single characters
-    // For example, letterCombinations(["a","b","c"]) would return the following:
-    // ["a","b","c","ab","ac","ba","bc","ca","cb","abc","acb","bac","bca","cab","cba"]
+function letterCombinations(a) {
+  // This function returns an array of all combinations of the given letters
+  // Input type: Array of single characters
+  // For example, letterCombinations(["a","b","c"]) would return the following:
+  // ["a","b","c","ab","ac","ba","bc","ca","cb","abc","acb","bac","bca","cab","cba"]
+  if (a.length === 1) {
+    return [a[0]];
+  }
+  let all = [];
+  for (let i = 0; i < a.length; i++) {
+    const currentLetter = a[i];
+    const remainingLetters = a.slice(0, i) + a.slice(i + 1);
+    const combsOfRemainingLetters = letterCombinations(remainingLetters);
+    all.push(currentLetter)
+    combsOfRemainingLetters.forEach(subComb => {
+      let result = (currentLetter + subComb);
+      if (!(result.indexOf(',') > -1)) {
+        all.push(result);
+      }
+    });
+  }
+  return all;
 }
-
 
 module.exports = {
-    factorial,
-    fibonacci,
-    reverseString,
-    fizzBuzz,
-    findInArray,
-    sumArray,
-    findMax,
-    coinFlips,
-    letterCombinations,
-    multiplication
+  factorial,
+  fibonacci,
+  fizzBuzz,
+  reverseString,
+  multiplication,
+  sumArray,
+  findInArray,
+  findMax,
+  coinFlips,
+  letterCombinations,
 }
--- test/test.js
+++ test/test.js
@@ -2,125 +2,129 @@ const assert = require('assert');
 let pathToFuncitons = process.env.TEST_SOLUTIONS ? "../solutions.js" : '../problems.js'
 const fns = require(pathToFuncitons)
 
-describe('#factorial()', function() {
-    it('handles base case', function() {
-        assert.equal(fns.factorial(1), 1)
-    });
-    it('calculates factorials', function() {
-        assert.equal(fns.factorial(5), 120)
-        assert.equal(fns.factorial(8), 40320)
+describe('#factorial()', function () {
+  it('handles base case', function () {
+    assert.equal(fns.factorial(1), 1)
+  });
+  it('calculates factorials', function () {
+    assert.equal(fns.factorial(5), 120)
+    assert.equal(fns.factorial(8), 40320)
 
-    });
+  });
 });
 
-describe('#fibonacci()', function() {
-    it('handles base cases', function() {
-        assert.equal(fns.fibonacci(1), 1)
-        assert.equal(fns.fibonacci(2), 1)
+describe('#fibonacci()', function () {
+  it('handles base cases', function () {
+    assert.equal(fns.fibonacci(1), 1)
+    assert.equal(fns.fibonacci(2), 1)
 
-    });
-    it('calculates fibonacci numbers', function() {
-        assert.equal(fns.fibonacci(5), 5)
-        assert.equal(fns.fibonacci(10), 55)
-        assert.equal(fns.fibonacci(30), 832040)
-    });
+  });
+  it('calculates fibonacci numbers', function () {
+    assert.equal(fns.fibonacci(5), 5)
+    assert.equal(fns.fibonacci(10), 55)
+    assert.equal(fns.fibonacci(30), 832040)
+  });
 });
 
-describe('#reverseString()', function() {
-    it('handles base cases', function() {
-        assert.equal(fns.reverseString(""),"")
-        assert.equal(fns.reverseString("x"), "x")
-
-    });
-    it('reverses strings', function() {
-        assert.equal(fns.reverseString("abcdefg"), "gfedcba")
-        assert.equal(fns.reverseString("123"), "321")
-    });
-});
-describe('#sumArray()', function() {
-    it('handles base case', function() {
-        assert.equal(fns.sumArray([1]),1)
-    });
-    it('sums array values', function() {
-        assert.equal(fns.sumArray([1,1]),2)
-        assert.equal(fns.sumArray([1,2,3,4,5]),15)
-    });
+describe('#fizzBuzz()', function () {
+  it('calculates fizzBuzz', function () {
+    assert.equal(fns.fizzBuzz(3), "1, 2, fizz")
+    assert.equal(fns.fizzBuzz(5), "1, 2, fizz, 4, buzz")
+    assert.equal(fns.fizzBuzz(15), "1, 2, fizz, 4, buzz, fizz, 7, 8, fizz, buzz, 11, fizz, 13, 14, fizzBuzz")
+  });
 });
 
-describe('#findInArray()', function() {
-    it('handles base cases', function() {
-        assert.strictEqual(fns.findInArray([],'x'), false)
-        assert.strictEqual(fns.findInArray(['y'],'x'), false)
-        assert.strictEqual(fns.findInArray(['x'],'x'), true)
+describe('#reverseString()', function () {
+  it('handles base cases', function () {
+    assert.equal(fns.reverseString(""), "")
+    assert.equal(fns.reverseString("x"), "x")
 
-    });
-    it('determines inclusion/exclusion', function() {
-        assert.strictEqual(fns.findInArray(['x', 'y'],'x'), true)
-        assert.strictEqual(fns.findInArray(['y', 'x'],'x'), true)
-        assert.strictEqual(fns.findInArray(['xx', 'xx'],'x'), false)
-        assert.strictEqual(fns.findInArray(['a','b','c','d', 'e', 'f', 'g'],'x'), false)
-        assert.strictEqual(fns.findInArray(['a','b','x','d', 'e', 'f', 'g'],'x'), true)
-    });
+  });
+  it('reverses strings', function () {
+    assert.equal(fns.reverseString("abcdefg"), "gfedcba")
+    assert.equal(fns.reverseString("123"), "321")
+  });
 });
 
-describe('#multipliction()', function() {
-    it('handles base cases', function() {
-        assert.equal(fns.multiplication(0,0), 0)
-        assert.equal(fns.multiplication(0,1),0)
-        assert.equal(fns.multiplication(1,0),0)
-        assert.equal(fns.multiplication(1,1),1)
-        assert.equal(fns.multiplication(1,10),10)
-        assert.equal(fns.multiplication(10,1),10)
-
-
-    });
-    it('multiplies', function() {
-        assert.equal(fns.multiplication(10,10),100)
-        assert.equal(fns.multiplication(50, 2),100)
-        assert.equal(fns.multiplication(100, 100),10000)
-        assert.equal(fns.multiplication(23, 4),92)
-    });
+describe('#multiplication()', function () {
+  it('handles base cases', function () {
+    assert.equal(fns.multiplication(0, 0), 0)
+    assert.equal(fns.multiplication(0, 1), 0)
+    assert.equal(fns.multiplication(1, 0), 0)
+    assert.equal(fns.multiplication(1, 1), 1)
+    assert.equal(fns.multiplication(1, 10), 10)
+    assert.equal(fns.multiplication(10, 1), 10)
+  });
+  it('multiplies', function () {
+    assert.equal(fns.multiplication(10, 10), 100)
+    assert.equal(fns.multiplication(50, 2), 100)
+    assert.equal(fns.multiplication(100, 100), 10000)
+    assert.equal(fns.multiplication(23, 4), 92)
+  });
 });
 
-describe('#findMax()', function() {
-    it('handles base case', function() {
-        assert.equal(fns.findMax([1]),1)
-    });
-    it('finds max value', function() {
-        assert.equal(fns.findMax([1,1]),1)
-        assert.equal(fns.findMax([2,1]),2)
-        assert.equal(fns.findMax([1,2]),2)
-        assert.equal(fns.findMax([1,4,2,5,6,3,12,1]),12)
-
-    });
+describe('#sumArray()', function () {
+  it('handles base case', function () {
+    assert.equal(fns.sumArray([1]), 1)
+  });
+  it('sums array values', function () {
+    assert.equal(fns.sumArray([1, 1]), 2)
+    assert.equal(fns.sumArray([1, 2, 3, 4, 5]), 15)
+  });
 });
 
-describe('#coinflips()', function() {
-    it('calculates coin flips', function() {
-        assert.deepEqual(fns.coinFlips(1).sort(), ["T", "H"].sort())
-        assert.deepEqual(fns.coinFlips(2).sort(), ["TT", "HH", "TH", "HT"].sort())
-        assert.deepEqual(fns.coinFlips(3).sort(), 
-        ["TTT", "TTH", "THT", "THH", "HTT", "HTH", "HHT", "HHH" ].sort())
-    });
-    
+describe('#findInArray()', function () {
+  it('handles base cases', function () {
+    assert.strictEqual(fns.findInArray([], 'x'), false)
+    assert.strictEqual(fns.findInArray(['y'], 'x'), false)
+    assert.strictEqual(fns.findInArray(['x'], 'x'), true)
+  });
+  it('determines inclusion/exclusion', function () {
+    assert.strictEqual(fns.findInArray(['x', 'y'], 'x'), true)
+    assert.strictEqual(fns.findInArray(['y', 'x'], 'x'), true)
+    assert.strictEqual(fns.findInArray(['xx', 'xx'], 'x'), false)
+    assert.strictEqual(fns.findInArray(['a', 'b', 'c', 'd', 'e', 'f', 'g'], 'x'), false)
+    assert.strictEqual(fns.findInArray(['a', 'b', 'x', 'd', 'e', 'f', 'g'], 'x'), true)
+  });
 });
-describe('#coinflips()', function() {
-    it('calculates coin flips', function() {
-        assert.deepEqual(fns.coinFlips(1).sort(), ["T", "H"].sort())
-        assert.deepEqual(fns.coinFlips(2).sort(), ["TT", "HH", "TH", "HT"].sort())
-        assert.deepEqual(fns.coinFlips(3).sort(), 
-        ["TTT", "TTH", "THT", "THH", "HTT", "HTH", "HHT", "HHH" ].sort())
-    });
-});
-describe('#letterCombinations()', function() {
-    it('handles base case', function() {
-        assert.deepEqual(fns.letterCombinations(["a"]),["a"])
-    });
-    it('gets letter combinations', function() {
-        assert.deepEqual(
-            fns.letterCombinations(["a","b","c"]).sort(),
-            ["a","b","c","ab","ac","ba","bc","ca","cb","abc","acb","bac","bca","cab","cba"].sort()
-            )
-    });
 
+describe('#findMax()', function () {
+  it('handles base case', function () {
+    assert.equal(fns.findMax([1]), 1)
+  });
+  it('finds max value', function () {
+    assert.equal(fns.findMax([1, 1]), 1)
+    assert.equal(fns.findMax([2, 1]), 2)
+    assert.equal(fns.findMax([1, 2]), 2)
+    assert.equal(fns.findMax([1, 4, 2, 5, 6, 3, 12, 1]), 12)
+  });
+});
+
+describe('#coinflips()', function () {
+  it('calculates coin flips', function () {
+    assert.deepEqual(fns.coinFlips(1).sort(), ["T", "H"].sort())
+    assert.deepEqual(fns.coinFlips(2).sort(), ["TT", "HH", "TH", "HT"].sort())
+    assert.deepEqual(fns.coinFlips(3).sort(),
+      ["TTT", "TTH", "THT", "THH", "HTT", "HTH", "HHT", "HHH"].sort())
+  });
+});
+describe('#coinflips()', function () {
+  it('calculates coin flips', function () {
+    assert.deepEqual(fns.coinFlips(1).sort(), ["T", "H"].sort())
+    assert.deepEqual(fns.coinFlips(2).sort(), ["TT", "HH", "TH", "HT"].sort())
+    assert.deepEqual(fns.coinFlips(3).sort(),
+      ["TTT", "TTH", "THT", "THH", "HTT", "HTH", "HHT", "HHH"].sort())
+  });
+});
+
+describe('#letterCombinations()', function () {
+  it('handles base case', function () {
+    assert.deepEqual(fns.letterCombinations(["a"]), ["a"])
+  });
+  it('gets letter combinations', function () {
+    assert.deepEqual(
+      fns.letterCombinations(["a", "b", "c"]).sort(),
+      ["a", "b", "c", "ab", "ac", "ba", "bc", "ca", "cb", "abc", "acb", "bac", "bca", "cab", "cba"].sort()
+    )
+  });
 });
